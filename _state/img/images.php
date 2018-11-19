9c14d5a2fcb939b7998f35f03709379367cea76a
Christian Holzberger <ch@mosaiksoftware.de>
2018-11-19 02:52:52 +0100

    WEBSITE: add static images

---
+++ _state/img/images.php
@@ -0,0 +1,28 @@
+<?
+
+cssMenu("startseite", "nav");
+cssMenu("ueberuns", "nav");
+cssMenu("seminare", "nav");
+cssMenu("schulungszentrum", "nav");
+cssMenu("download", "nav");
+cssMenu("kontakt", "nav");
+cssMenu("impressum", "nav");
+cssMenu("sitemap", "nav");
+
+cssMenu("philosophie", "sub_nav");
+cssMenu("referenten", "sub_nav");
+cssMenu("partner", "sub_nav");
+cssMenu("referenzen", "sub_nav");
+
+cssMenu("arbeitssicherheit", "sub_nav");
+cssMenu("dichtheit", "sub_nav");
+cssMenu("kanalreinigung", "sub_nav");
+cssMenu("kanalsanierung", "sub_nav");
+cssMenu("kanalinspektion", "sub_nav");
+cssMenu("abscheide", "sub_nav");
+
+cssMenu("kontaktformular", "sub_nav");
+cssMenu("callbackformular", "sub_nav");
+cssMenu("anfahrt", "sub_nav");
+cssMenu("anschrift", "sub_nav");
+?>
