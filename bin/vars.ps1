25cb5d844ffd4861c69013ec898cf7016fd07035
Chrisitan Stichler <cstichler@googlemail.com>
2018-12-01 00:17:31 +0100

    FlexTable-conflict behoben

--- bin/vars.ps1
+++ bin/vars.ps1
@@ -3,11 +3,10 @@
 $env:BUILD_VERSION="20181128"
 $env:BUILD_VERSION_WEB=$env:BUILD_VERSION
 $env:BUILD_VERSION_MYSQL=$env:BUILD_VERSION
-$env:BUILD_BRANCH="prod"
-$env:RELEASE_BRANCH="prod"
+$env:BUILD_BRANCH="dev"
+$env:RELEASE_BRANCH="dev"
 
 $env:PROJECT_HOME="c:/projekte/sag-new"
-$env:PROJECT_HOME="/home/cholzberger/Projekte/SAG-Akademie-GmbH/sag-new"
 $dc_args = @("-p", $env:RELEASE_BRANCH,
 "-f", "../docker-compose.yml", 
 "-f", "../docker-compose/build-$env:BUILD_BRANCH.yml",
