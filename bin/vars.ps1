e4f9b798bd43063bc843e425c40332c38caed9a7
Chrisitan Stichler <cstichler@googlemail.com>
2019-02-28 23:37:17 +0100

    /modules/terminBearbeiten/index.html um die Bereiche Quallifikationsart und Rezertifizierungszeit erweitert

--- bin/vars.ps1
+++ bin/vars.ps1
@@ -6,7 +6,7 @@ $env:BUILD_VERSION_MYSQL=$env:BUILD_VERSION
 $env:BUILD_BRANCH="prod"
 $env:RELEASE_BRANCH="prod"
 
-$env:PROJECT_HOME="c:/projekte/sag-new"
+$env:PROJECT_HOME="c:/projekte/sag-new-tmp"
 $env:PROJECT_HOME="/home/cholzberger/Projekte/SAG-Akademie-GmbH/sag-new"
 $dc_args = @("-p", $env:RELEASE_BRANCH,
 "-f", "../docker-compose.yml", 
